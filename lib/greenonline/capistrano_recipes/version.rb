7088e322d06f364d00cbb42446e8afcdbd8d655d
Pieter Visser <pieter@pietervisser.nl>
2013-06-26 00:15:47 +0200

    update unicorn config in current release path instead of current path

--- lib/greenonline/capistrano_recipes/version.rb
+++ lib/greenonline/capistrano_recipes/version.rb
@@ -1,5 +1,5 @@
 module Greenonline
   module CapistranoRecipes
-    VERSION = "0.0.8"
+    VERSION = "0.0.9"
   end
 end
--- lib/greenonline/recipes/unicorn.rb
+++ lib/greenonline/recipes/unicorn.rb
@@ -3,6 +3,7 @@ module Capistrano
     set_default(:unicorn_user)    { user }
     set_default(:unicorn_pid)     { "#{current_path}/tmp/pids/unicorn.pid" }
     set_default(:unicorn_config)  { "#{current_path}/config/unicorn.rb" }
+    set_default(:unicorn_config_write)  { "#{current_release}/config/unicorn.rb" }
     set_default(:unicorn_log)     { "#{current_path}/log/unicorn.log" }
     set_default(:unicorn_workers) { 2 }
 
@@ -10,7 +11,7 @@ module Capistrano
     namespace :unicorn do
       desc "update latest unicorn configuration"
       task :update_config, roles: :web do
-        template 'unicorn.rb.erb', unicorn_config
+        template 'unicorn.rb.erb', unicorn_config_write
       end
       after "deploy:update_code", "unicorn:update_config"
 
