ed7ff870f90e03a9263b37fd539c3d1d6cb6bc89
Pieter Visser <pieter@pietervisser.nl>
2013-06-25 22:14:00 +0200

    added unicorn init template and service tasks

--- lib/greenonline/recipes/unicorn.rb
+++ lib/greenonline/recipes/unicorn.rb
@@ -2,7 +2,7 @@ module Capistrano
   Configuration.instance(true).load do
     set_default(:unicorn_user)    { user }
     set_default(:unicorn_pid)     { "#{current_path}/tmp/pids/unicorn.pid" }
-    set_default(:unicorn_config)  { "#{shared_path}/config/unicorn.rb" }
+    set_default(:unicorn_config)  { "#{current_release}/config/unicorn.rb" }
     set_default(:unicorn_log)     { "#{shared_path}/log/unicorn.log" }
     set_default(:unicorn_workers) { 2 }
 
@@ -10,9 +10,20 @@ module Capistrano
     namespace :unicorn do
       desc "update latest unicorn configuration"
       task :update_config, roles: :web do
-        template 'unicorn.rb.erb', File.join(current_release, '/config/unicorn.rb')
+        template 'unicorn.rb.erb', unicorn_config
       end
       after "deploy:update_code", "unicorn:update_config"
+
+      desc "Setup Unicorn initializer and app configuration"
+      task :setup, roles: :app do
+        run "mkdir -p #{shared_path}/config"
+        template "unicorn_init.erb", "/tmp/unicorn_init"
+        run "chmod +x /tmp/unicorn_init"
+        surun "mv /tmp/unicorn_init /etc/init.d/unicorn_#{application.downcase}"
+        surun "chkconfig --add unicorn_#{application.downcase}"
+        surun "chkconfig unicorn_#{application.downcase} on"
+      end
+      after "deploy:setup", "unicorn:setup"
 
       namespace :server do
         %w(start stop restart).each do |command|
