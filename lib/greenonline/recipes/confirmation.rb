7a6093a4c27cd8c5337403a8dea352b29295fc04
Peter de Ruijter <peter@l2code.nl>
2015-01-20 15:16:12 +0100

    Adds confirmation for deployment.

---
+++ lib/greenonline/recipes/confirmation.rb
@@ -0,0 +1,26 @@
+module Capistrano
+  Configuration.instance(true).load do
+    set_default(:ask_confirmation?, false)
+
+    before :deploy, :deploy_confirmation
+    task :deploy_confirmation do
+      next if !ask_confirmation?
+      set :confirmed, proc {
+        puts <<-WARN
+
+      ========================================================================
+                WARNING: You're about to deploy to #{stage}
+      ========================================================================
+
+        WARN
+        set(:answer, Capistrano::CLI.ui.ask("Are you sure you want to continue? Type '#{stage}'") )
+        if fetch(:answer) =~ /#{stage}/ then true else false end
+      }.call
+
+      if !fetch(:confirmed)
+        puts "\nDeploy halted!"
+        exit
+      end
+    end
+  end
+end
